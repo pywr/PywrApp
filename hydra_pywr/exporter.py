569ecb37bced158170e4f4a395d629ab05d5c386
James Tomlinson <tomo.bbe@gmail.com>
2018-04-11 10:02:45 +0100

    Initial commit of exporter.

---
+++ hydra_pywr/exporter.py
@@ -0,0 +1,147 @@
+import json
+from past.builtins import basestring
+
+
+class PywrHydraExporter:
+    def __init__(self, data, attributes, attribute_group_items):
+        self.data = data
+        self.attributes = attributes
+        self.attribute_group_items = attribute_group_items
+
+    def get_pywr_data(self):
+
+        pywr_data = {}
+
+        for group_name in ('metadata', 'timestepper', 'recorders', 'parameters'):
+            # Recorders and parameters are JSON encoded.
+            decode_from_json = group_name in ('recorders', 'parameters')
+
+            group_data = {}
+            for key, value in self.generate_group_data(group_name, decode_from_json=decode_from_json):
+                group_data[key] = value
+
+            # Only make the section if it contains data.
+            if len(group_data) > 0:
+                pywr_data[group_name] = group_data
+
+        nodes = []
+        for node in self.generate_pywr_nodes():
+            nodes.append(node)
+        pywr_data['nodes'] = nodes
+
+        edges = []
+        for edge in self.generate_pywr_edges():
+            edges.append(edge)
+        pywr_data['edges'] = edges
+
+        return pywr_data
+
+    def _get_attribute_group_from_name(self, group_name):
+
+        for attribute_group_item in self.attribute_group_items:
+            group = attribute_group_item['group']
+            if group['name'] == group_name:
+                return group
+
+        raise ValueError('No attribute group found for group name: {}'.format(group_name))
+
+    def _get_attributes_for_group_from_name(self, group_name):
+
+        group = self._get_attribute_group_from_name(group_name)
+
+        for attribute_group_item in self.attribute_group_items:
+            if attribute_group_item['group_id'] == group['id']:
+                yield self.attributes[attribute_group_item['attr_id']]
+
+    def _get_resource_scenario(self, resource_attribute_id):
+
+        # TODO this just returns the first resource scenario that is found.
+        for scenario in self.data['scenarios']:
+            for resource_scenario in scenario['resourcescenarios']:
+                if resource_scenario['resource_attr_id'] == resource_attribute_id:
+                    return resource_scenario
+
+        raise ValueError('No resource scenario found for resource attribute id: {}'.format(resource_attribute_id))
+
+    def _get_node(self, node_id):
+
+        for node in self.data['nodes']:
+            if node['id'] == node_id:
+                return node
+
+        raise ValueError('No node found with node_id: {}'.format(node_id))
+
+    def generate_pywr_nodes(self):
+        """ Generator returning a Pywr dict for each node in the network. """
+
+        for node in self.data['nodes']:
+
+            # Create the basic information.
+            pywr_node = {'name': node['name']}
+
+            if node['description'] is not None:
+                pywr_node['comment'] = node['description']
+
+            # Then add any corresponding attributes / data
+            for resource_attribute in node['attributes']:
+                attribute = self.attributes[resource_attribute['attr_id']]
+                resource_scenario = self._get_resource_scenario(resource_attribute['resource_attr_id'])
+                dataset = resource_scenario['dataset']
+                value = dataset['value']
+
+                if isinstance(value, basestring):
+                    pywr_node[attribute['name']] = json.loads(value)
+                else:
+                    pywr_node[attribute['name']] = value
+
+            yield pywr_node
+
+    def generate_pywr_edges(self):
+        """ Generator returning a Pywr tuple for each link/edge in the network. """
+
+        for link in self.data['links']:
+            node_from = self._get_node(link['node_1_id'])
+            node_to = self._get_node(link['node_2_id'])
+            yield [node_from['name'], node_to['name']]
+
+    def generate_group_data(self, group_name, decode_from_json=False):
+        """ Generator returning a key and dict value for meta keys. """
+
+        # These are all the attributes associated with the group
+        try:
+            group_attributes = list(self._get_attributes_for_group_from_name(group_name))
+        except ValueError:
+            # No group exists so there is no data
+            return
+
+        for resource_attribute in self.data['attributes']:
+
+            attribute = self.attributes[resource_attribute['attr_id']]
+
+            attribute_name = attribute['name']
+
+            for group_attribute in group_attributes:
+                # Find if this attribute is in this group
+                if group_attribute['id'] == attribute['id']:
+                    break
+            else:
+                continue  # Filter out keys not associated with meta_keys
+
+            resource_scenario = self._get_resource_scenario(resource_attribute['resource_attr_id'])
+            dataset = resource_scenario['dataset']
+            value = dataset['value']
+
+            if decode_from_json:
+                value = json.loads(value)
+
+            # Hydra opportunistically converts everything to native types
+            # Some of the Pywr data should remain as string despite looking like a float/int
+            if attribute_name == 'minimum_version' and group_name == 'metadata':
+                value = str(value)
+
+            yield attribute_name, value
+
+
+
+
+
---
+++ tests/test_hydra_base_exporting.py
@@ -0,0 +1,92 @@
+"""
+The unit tests in here interact directly with hydra-base (rather than using the Web API).
+"""
+from helpers import *
+from fixtures import *
+from hydra_base_fixtures import *
+from hydra_pywr.importer import PywrHydraImporter
+from hydra_pywr.exporter import PywrHydraExporter
+from hydra_pywr.template import generate_pywr_attributes, generate_pywr_template
+import hydra_base
+import pytest
+import json
+
+
+def assert_identical_pywr_data(data1, data2):
+    """ Assert two Pywr JSON data dictionaries are identical. """
+    assert sorted(data1.keys()) == sorted(data2.keys())
+
+    for key in data1.keys():
+        if key == 'nodes':
+            # The ordering of these lists does not matter to Pywr
+            assert sorted(data1[key], key=lambda n: n['name']) == sorted(data2[key], key=lambda n: n['name'])
+        elif key == 'edges':
+            # The ordering of these lists does not matter to Pywr
+            assert sorted(data1[key]) == sorted(data2[key])
+        else:
+            assert data1[key] == data2[key]
+
+
+@pytest.fixture()
+def db_with_pywr_network(pywr_json_filename, session, projectmaker, root_user_id):
+    project = projectmaker.create()
+
+    importer = PywrHydraImporter(pywr_json_filename)
+
+    # First create the Pywr specific attribute groups.
+    attribute_group_ids = {}
+    for group_data in importer.add_attribute_group_request_data(project.id):
+        response_group = hydra_base.add_attribute_group(JSONObject(group_data), user_id=root_user_id)
+        attribute_group_ids[group_data['name']] = response_group.id
+
+    # First the attributes must be added.
+    attributes = [JSONObject(a) for a in importer.add_attributes_request_data()]
+
+    # The response attributes have ids now.
+    response_attributes = hydra_base.add_attributes(attributes)
+
+    # Convert to a simple dict for local processing.
+    attribute_ids = {a.attr_name: a.attr_id for a in response_attributes}
+
+    # Now we try to create the network
+    network = importer.add_network_request_data(attribute_ids, project.id)
+    json_network = convert_network_to_json_object(network)
+
+    hydra_network = hydra_base.add_network(json_network, user_id=root_user_id)
+
+    # Now we have to add the attribute group items
+    attribute_group_items = importer.add_attribute_group_items_request_data(attribute_ids, attribute_group_ids,
+                                                                            hydra_network.id)
+
+    hydra_base.add_attribute_group_items([JSONObject(i) for i in attribute_group_items], user_id=root_user_id)
+
+    return hydra_network.id, pywr_json_filename
+
+
+def test_export(db_with_pywr_network, root_user_id):
+    pywr_network_id, pywr_json_filename = db_with_pywr_network
+
+    # Fetch the network
+    network = hydra_base.get_network(pywr_network_id, user_id=root_user_id, include_data='Y')
+    # Fetch all the attributes
+    attributes = hydra_base.get_attributes()
+    # TODO not sure why this returns SQLAlchemy object?
+    attributes = {attr.attr_id: JSONObject(attr) for attr in attributes}
+
+    # Fetch all the attribute group items for this network
+    attribute_group_items = hydra_base.get_network_attributegroup_items(pywr_network_id, user_id=root_user_id)
+    attribute_group_items = [JSONObject(a) for a in attribute_group_items]
+
+    print(type(network))
+
+    exporter = PywrHydraExporter(network, attributes, attribute_group_items)
+
+    pywr_data_exported = exporter.get_pywr_data()
+
+    # Check transformed data is about right
+    with open(pywr_json_filename) as fh:
+        pywr_data = json.load(fh)
+
+    assert_identical_pywr_data(pywr_data, pywr_data_exported)
+
+
