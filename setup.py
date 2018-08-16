4580951fec463f2a22a948d47d5ebe9c184d7410
James Tomlinson <tomo.bbe@gmail.com>
2018-08-16 16:01:54 +0100

    Use MANIFEST.in and include_package_data=True rather than package_data.

--- setup.py
+++ setup.py
@@ -6,10 +6,7 @@ setup(
     version='0.1',
     description='Hydra plugins and library for import and exporting Pywr models.',
     packages=['hydra_pywr'],
-    package_data={
-        # Include all the gzipped model
-        'hydra_pywr': ['node_layouts.json', 'pywr-app-service.yml'],
-    },
+    include_package_data=True,
     entry_points='''
     [console_scripts]
     hydra-pywr=hydra_pywr.cli:start_cli
